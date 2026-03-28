b4e3d7be63368fa4d33323b93a4e9ae291fe6284
Adebola Adefemi <adebolaadefemi@yahoo.com>
2026-03-28 02:29:56 +0100

    Update auth and login template

--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,15 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from app import login_manager
 from app.models import User
 
 auth = Blueprint("auth", __name__)
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 @auth.route("/", methods=["GET", "POST"])
 @auth.route("/login", methods=["GET", "POST"])
@@ -16,13 +18,16 @@ def login():
         email = request.form.get("email")
         password = request.form.get("password")
 
-        user = User.query.filter_by(email=email, password=password).first()
+        user = User.query.filter_by(email=email).first()
 
-        if user:
+        if user and user.password == password:
             login_user(user)
             return redirect(url_for("main.dashboard"))
+        else:
+            flash("Invalid email or password", "danger")
 
     return render_template("login.html")
+
 
 @auth.route("/logout")
 @login_required
