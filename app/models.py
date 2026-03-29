9a3d18d76399f240bf36b2efd7419cee9cefbb4a
Adebola Adefemi <adebolaadefemi@yahoo.com>
2026-03-29 22:43:10 +0100

    Add activity templates and confirm ActivityLog model

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(50), default="staff")
 
 
